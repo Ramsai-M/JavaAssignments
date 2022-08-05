9ba904c4f503437d081ed642ad7c258be2024020
Ramsai-M <ramsaimuthyala97@gmail.com>
2022-08-05 09:30:29 +0530

    Print -10 to 1 numbers using While Loop

---
+++ PrntM10to1WP.java
@@ -0,0 +1,12 @@
+//Print -10 to 1 numbers using While Loop
+public class PrntM10to1WP
+{
+	public static void main(String[] args) {
+	    //Scanner reader=new Scanner(System.in);
+		int i=-10;
+		while(i<=1){
+		    System.out.println(i);
+		    i++;
+		}
+	}
+}
