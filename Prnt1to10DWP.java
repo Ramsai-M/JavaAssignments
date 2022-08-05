30bf160b3988e4c279d939b589ddb7387a7281f8
Ramsai-M <ramsaimuthyala97@gmail.com>
2022-08-05 09:31:59 +0530

    Print 1 to 10 numbers using do while loop

---
+++ Prnt1to10DWP.java
@@ -0,0 +1,12 @@
+//Print 1 to 10 numbers using do while loop
+public class Prnt1to10DWP
+{
+	public static void main(String[] args) {
+	    //Scanner reader=new Scanner(System.in);
+		int i=1;
+		do{
+		    System.out.println(i);
+		    i++;
+		}while(i<=10);
+	}
+}
